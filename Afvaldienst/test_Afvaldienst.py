1154b53dd5267578709072734654966237059d71
Bram van Dartel <spam@rootrulez.com>
2020-02-12 00:33:05 +0100

    add additional option to count from today or from tomorrow

---
+++ Afvaldienst/test_Afvaldienst.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from Afvaldienst import Afvaldienst
+
+trash = Afvaldienst('mijnafvalwijzer', '5146EG', '6', '', 'no')
+print("\n")
+print(trash.trash_schedulefull_json)
+print("\n")
+print(trash.trash_schedule_next_days_json)
+print("\n")
+print(trash.trash_schedule_today_json)
+print("\n")
+print(trash.trash_schedule_tomorrow_json)
+print("\n")
+print(trash.trash_schedule_dat_json)
+print("\n")
+print(trash.trash_schedule_next_item_json)
+print("\n")
+print(trash.trash_schedule_next_date_json)
+print("\n")
+print(trash.trash_type_list)

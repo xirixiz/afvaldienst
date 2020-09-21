992ac270be46dfe7655bb4e438dc3f8c51a07daa
Bram van Dartel <root@rootrulez.com>
2020-09-21 10:30:35 +0200

    add scraper

--- Afvaldienst/__init__.py
+++ Afvaldienst/__init__.py
@@ -3,6 +3,7 @@ __author_email__ = 'spam@rootrulez.com'
 __license__ = 'MIT'
 __maintainer_email__ = 'spam@rootrulez.com'
 __url__ = 'https://github.com/xirixiz/afvaldienst',
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 
 from .Afvaldienst import Afvaldienst
+from .AfvaldienstScraper import AfvaldienstScraper
